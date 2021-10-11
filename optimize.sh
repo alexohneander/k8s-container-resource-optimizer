cb4d92ebc6f94a894faefbd637e62873272365bc
Alex Wellnitz <moin@wellnitz-alex.de>
2021-10-11 12:54:26 +0200

    Get namespaces, pods, resource requests and resource limits.

--- optimize.sh
+++ optimize.sh
@@ -9,8 +9,33 @@ NAMESPACES=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
 echo "Checking following Namespaces: "
 # loop through namespaces
 for namespace in $NAMESPACES; do
-    echo "Namespace: $namespace"
-    
+
     # get pods from namespace
     PODS=$(kubectl get pods -n $namespace -o jsonpath='{.items[*].metadata.name}')
+
+    # if PODS is empty, skip namespace
+    if [ -z "$PODS" ]; then
+        echo "No Pods found in namespace $namespace"
+        continue
+    fi
+
+    echo "Checking Pods in namespace $namespace"
+    for pod in $PODS; do
+
+        # if pod has no limits or request, skip pod
+        if [ -z "$(kubectl describe pod $pod -n $namespace | grep Limits)" ] || [ -z "$(kubectl describe pod $pod -n $namespace | grep Requests)" ]; then
+            continue
+        fi
+
+        # get resource requests/limits from pod
+        REQUESTS=$(kubectl get pod $pod -n $namespace -o jsonpath='{.spec.containers[*].resources.requests}')
+        LIMITS=$(kubectl get pod $pod -n $namespace -o jsonpath='{.spec.containers[*].resources.limits}')
+        
+        echo " "
+        echo "Pod: $pod"
+        echo "-----------------------"
+        echo "Requests: $REQUESTS"
+        echo "Limits: $LIMITS"
+        
+    done
 done
